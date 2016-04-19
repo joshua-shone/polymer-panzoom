16896bec519ac2db880b643601b26fdb91fea51b
Joshua Shone <josh.shone@gmail.com>
2016-04-19 10:57:19 +0200

    Initial commit including demo

---
+++ demo.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var http    = require('http');
+var yargs   = require('yargs');
+var path    = require('path');
+
+var app = express();
+var server = http.Server(app);
+
+app.use(express.static('.'));
+app.use(express.static('../../'));
+
+app.use(function(req, res, next) {
+  if (req.path === '/') {
+    res.sendFile(path.join(__dirname, './demo.html'));
+  } else {
+    next();
+  }
+});
+
+var port = yargs.argv['port'] || 8080;
+
+server.listen(port, function() {
+  console.log('Demo running on port ' + port);
+});
